e6cd1bfa61aef5206b5a0cf4314ed866fbbe505a
Rivindu <rivinduprabha@gmail.com>
2023-07-25 01:57:47 +0530

    favs

--- script/favourites.js
+++ script/favourites.js
@@ -1,25 +1,35 @@
 function loadXMLDoc() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
-      alert(xmlhttp.readyState);
+      // alert(xmlhttp.readyState);
       if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        myFunction(xmlhttp);
+        setHTML(xmlhttp);
       }
     };
     xmlhttp.open("GET", "script/favourites.xml", true);
     xmlhttp.send();
   }
-  function myFunction(xml) {
-    var i;
+  function setHTML(xml) {
     var xmlDoc = xml.responseXML;
-    var table="<tr><th>Title</th><th>Author</th></tr>";
+    var favs="";
     var x = xmlDoc.getElementsByTagName("song");
+    var y;
+
     for (i = 0; i <x.length; i++) { 
-      table += "<tr><td>" +
-      x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue +
-      "</td><td>" +
-      x[i].getElementsByTagName("artist")[0].childNodes[0].nodeValue +
-      "</td></tr>";
+      y=x[i].childNodes;
+
+      console.log(x[i].childNodes);
+
+      favs += "<div class='item'><div class='title'>" +
+      y[1].innerHTML +
+      "</div><img src='" + y[9].innerHTML +"' alt='"+y[1].innerHTML + "' height='160px' width='160px'>"+
+      "<div class='desc'>" +
+      "By "+y[3].innerHTML+
+      "<br>Year :"+y[5].innerHTML+
+      "<br>Genre :"+y[7].innerHTML+
+      "</div></div>";
+
     }
-    document.getElementById("fav_table").innerHTML = table;
+
+    document.getElementsByClassName("div_container")[0].innerHTML = favs;
   }
